ed1d85118da94056ec1d0f6ccb6dc7827be57139
robby <robby.remmenga@gmail.com>
2019-10-08 13:20:59 -0700

    initial commit

---
+++ src/main/java/com/example/pizzareviewwebsite/PizzaReviewWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzareviewwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaReviewWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaReviewWebsiteApplication.class, args);
+	}
+
+}
